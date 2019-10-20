0e3aee3ebf4da72f3b1478e2c8cb50ec1676fdc2
Eli Selkin <eli.j.selkin@gmail.com>
2019-10-19 18:13:45 -0700

    change POSTGRES_HOSTNAME to POSTGRES_HOST and update documentation and add CHANGELOG

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="engage_scraper",
-    version="0.0.20",
+    version="0.0.27",
     author="Engage",
     author_email="eli.j.selkin@gmail.com",
     description="An agenda scraper framework for municipalities",
